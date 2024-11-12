4a589a98f2fa63e7ffa1cc6df74906f474948cee
hurtadoalejo <alejohg2911@gmail.com>
2024-11-12 16:28:48 -0500

    Cambios pendientes

---
+++ proyecto_final_concesionario_javafx/src/main/java/co/edu/uniquindio/poo/controller/IngresarAdministradoController.java
@@ -0,0 +1,15 @@
+package co.edu.uniquindio.poo.controller;
+
+import co.edu.uniquindio.poo.model.Concesionario;
+
+public class IngresarAdministradoController {
+    Concesionario concesionario;
+
+    public IngresarAdministradoController(Concesionario concesionario) {
+        this.concesionario = concesionario;
+    }
+
+    public boolean autenticarCredenciales(){
+        concesionario
+    }
+}
